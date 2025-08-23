10bc6e03b35722521b36ee0e4cd4dd193c374b2d
Bhagaban Chandra Biswal <bhagaban@Bhagabans-MacBook-Pro.local>
2025-08-23 10:58:22 +0530

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,26 @@
+my_dic = {
+    "name":"bhagaban",
+    "age":22,
+    "gender":"male",
+    "height":70,
+    "weight":80,
+}
+# print(my_dic)
+# print(my_dic['name'])
+# print(my_dic["age"])
+# print(my_dic.get("age"))
+# print(my_dic.get("gender"))
+# print(my_dic.get("height"))
+
+# my_dic.update({"age":100})
+# print(my_dic)
+#
+# my_dic["age"] = 200
+# print(my_dic)
+
+my_dic["test"] = "test"
+print(my_dic)
+
+
+
+
